2e25730e7ffc7dc28873de782f4966dfb21deaff
thisdcw <thisdcw@outlook.com>
2024-08-20 14:27:30 +0800

    初始化

---
+++ src/main/java/com/cw/Main.java
@@ -0,0 +1,11 @@
+package com.cw;
+
+/**
+ * @author thisdcw-com
+ * @date 2024/8/20 13:54
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
